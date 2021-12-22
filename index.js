16f164a8fbb76d92792048799543012f27ce5ff1
H.Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2021-12-23 02:56:59 +0300

    Throw error proper way

--- index.js
+++ index.js
@@ -12,27 +12,27 @@ async function run() {
     const packageVersion = packageJson.version;
 
     if (!(packageVersion && packageVersion.length !== 0))
-      throw 'Package version is empty.';
+      throw new Error('Package version is empty.');
 
     core.info('Check package version');
     const isVersionExist = await exec.exec('git', ['ls-remote', '--exit-code', '--tags', 'origin', packageVersion]);
     if (isVersionExist == 0)
-      throw 'Same version already exists: ' + packageVersion;
+      throw new Error('Same version already exists: ' + packageVersion);
 
     core.info('Commit changes to upm branch');
     var returnValue = await exec.exec('git', ['subtree', 'split', '--prefix=Packages/' + packageName, '--branch', upmBranch]);
     if (returnValue != 0)
-      throw 'Commit changes to upm [' + upmBranch + '] branch failed: ' + string(returnValue);
+      throw new Error('Commit changes to upm [' + upmBranch + '] branch failed: ' + string(returnValue));
 
     core.info('Create version tag with: ' + packageVersion);
     returnValue = await exec.exec('git', ['tag', packageVersion, upmBranch]);
     if (returnValue != 0)
-      throw 'Creating version tag [' + packageVersion + '] failed: ' + string(returnValue);
+      throw new Error('Creating version tag [' + packageVersion + '] failed: ' + string(returnValue));
 
     core.info('Push version and upm branch');
     returnValue = await exec.exec('git', ['push', 'origin', upmBranch, '--tags', '--force']);
     if (returnValue != 0)
-      throw 'Pushing version [' + packageVersion + '] and upm [' + upmBranch + '] branch failed: ' + string(returnValue);
+      throw new Error('Pushing version [' + packageVersion + '] and upm [' + upmBranch + '] branch failed: ' + string(returnValue));
   } catch (error) {
     core.setFailed(error.message);
   }
