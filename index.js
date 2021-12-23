c7e8bcafcece326948e0b02126a1aa2f968dd59f
H.Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2021-12-23 03:04:10 +0300

    Do not fail immediately if exec fails

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ async function run() {
   try {
     const packageName = core.getInput('name');
     const upmBranch = core.getInput('upmBranch');
-    
+    const execOptions = {ignoreReturnCode:true};
+
     const packageJsonBuffer = fs.readFileSync('Packages/' + packageName + '/package.json', {encoding:'utf8', flag:'r'});
     const packageJson = JSON.parse(packageJsonBuffer.toString());
     const packageVersion = packageJson.version;
@@ -15,22 +16,22 @@ async function run() {
       throw new Error('Package version is empty.');
 
     core.info('Check package version');
-    const isVersionExist = await exec.exec('git', ['ls-remote', '--exit-code', '--tags', 'origin', packageVersion]);
+    const isVersionExist = await exec.exec('git', ['ls-remote', '--exit-code', '--tags', 'origin', packageVersion], execOptions);
     if (isVersionExist == 0)
       throw new Error('Same version already exists: ' + packageVersion);
 
     core.info('Commit changes to upm branch');
-    var returnValue = await exec.exec('git', ['subtree', 'split', '--prefix=Packages/' + packageName, '--branch', upmBranch]);
+    var returnValue = await exec.exec('git', ['subtree', 'split', '--prefix=Packages/' + packageName, '--branch', upmBranch], execOptions);
     if (returnValue != 0)
       throw new Error('Commit changes to upm [' + upmBranch + '] branch failed: ' + string(returnValue));
 
     core.info('Create version tag with: ' + packageVersion);
-    returnValue = await exec.exec('git', ['tag', packageVersion, upmBranch]);
+    returnValue = await exec.exec('git', ['tag', packageVersion, upmBranch], execOptions);
     if (returnValue != 0)
       throw new Error('Creating version tag [' + packageVersion + '] failed: ' + string(returnValue));
 
     core.info('Push version and upm branch');
-    returnValue = await exec.exec('git', ['push', 'origin', upmBranch, '--tags', '--force']);
+    returnValue = await exec.exec('git', ['push', 'origin', upmBranch, '--tags', '--force'], execOptions);
     if (returnValue != 0)
       throw new Error('Pushing version [' + packageVersion + '] and upm [' + upmBranch + '] branch failed: ' + string(returnValue));
   } catch (error) {
